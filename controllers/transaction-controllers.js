e6fc54699b6a857d76be9508e92004baa8df69f6
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-12-28 09:29:09 +0100

    fix type of transaction info

--- controllers/transaction-controllers.js
+++ controllers/transaction-controllers.js
@@ -8,89 +8,85 @@ import Transaction from "../models/transaction.js";
 import { createMonthStats } from "./transactionsHelpers.js";
 
 export const addTransaction = async (req, res, next) => {
+  const { description, amount, date, category, typeOfTransaction } = req.body;
+  const owner = req.user.id;
 
-    const { description, amount, date, category, typeOfTransaction} = req.body;
-    const owner = req.user.id;
-    
-    try {
-        const user = await User.findById(owner);
+  try {
+    const user = await User.findById(owner);
 
-        if (!user) {
-            return res.status(404).json({ error: 'Invalid user or session' });
-        }
-        const newTransaction = new Transaction({
-            typeOfTransaction,
-            description,
-            amount,
-            date,
-            category, 
-            owner,
-        });
+    if (!user) {
+      return res.status(404).json({ error: "Invalid user or session" });
+    }
+    const newTransaction = new Transaction({
+      typeOfTransaction,
+      description,
+      amount,
+      date,
+      category,
+      owner,
+    });
 
-        await newTransaction.save(); 
+    await newTransaction.save();
 
-        if (typeOfTransaction === "income") {
-            user.totalIncome += amount;
-        } else if (typeOfTransaction === "expense") {
-            user.totalExpense += amount;
-            
-        } 
+    if (typeOfTransaction === "income") {
+      user.totalIncome += amount;
+    } else if (typeOfTransaction === "expense") {
+      user.totalExpense += amount;
+    }
 
-        user.newBalance = user.totalIncome - user.totalExpense;
+    user.newBalance = user.totalIncome - user.totalExpense;
 
-        await user.save();
+    await user.save();
 
     return res.status(200).json({
-        "newBalance": user.newBalance,
-        "transaction": {
-            "description": newTransaction.description,
-            "amount": newTransaction.amount,
-            "date": newTransaction.date,
-            "category": newTransaction.category,
-            "_id": newTransaction._id
-        }
-    })
-    
-    } catch (error) {
-
-    if (error.name === 'ValidationError') {
-            return res.status(400).json({ error: error.message });
-        }
-    next(error)
-}
-}
-
-export const getAllIncomess = async(req, res, next) => {
-  try {
-      const incomes = await Transaction.find({
-          "typeOfTransaction": "income",
-          "owner": req.user.id
-      });
-      const monthStats = createMonthStats(incomes);
-      res.status(200).json({
-          "incomes": incomes,
-          "monthStats": monthStats
-      });
-    } catch (error) {
-        next(error)
+      newBalance: user.newBalance,
+      transaction: {
+        description: newTransaction.description,
+        amount: newTransaction.amount,
+        date: newTransaction.date,
+        category: newTransaction.category,
+        _id: newTransaction._id,
+      },
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({ error: error.message });
     }
-}
+    next(error);
+  }
+};
 
-export const getAllExpensess = async(req, res, next) => {
-try {
-    const expenses = await Transaction.find({ 
-          "typeOfTransaction": "expense",
-          "owner": req.user.id
+export const getAllIncomess = async (req, res, next) => {
+  try {
+    const incomes = await Transaction.find({
+      typeOfTransaction: "income",
+      owner: req.user.id,
+    });
+    const monthStats = createMonthStats(incomes);
+    res.status(200).json({
+      incomes: incomes,
+      monthStats: monthStats,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllExpensess = async (req, res, next) => {
+  try {
+    const expenses = await Transaction.find({
+      typeOfTransaction: "expense",
+      owner: req.user.id,
     });
     const monthStats = createMonthStats(expenses);
     res.status(200).json({
-        "expense": expenses,
-        "monthStats": monthStats
-      });
-    } catch (error) {
-        next(error)
-    }
-}
+      expense: expenses,
+      monthStats: monthStats,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteTransaction = async (req, res, next) => {
   const { id } = req.params;
